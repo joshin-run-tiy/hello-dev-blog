65a9135990eb87d7417d5d663af02729927f889f
joshin-run <jahmsc@gmail.com>
2017-05-04 15:37:22 -0600

    Fix (Sidebar.js) Refactor

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -12,55 +12,23 @@ class Sidebar extends Component {
 
   render() {
     const postsSBArr = this.state.posts.posts;
-
-    function filter(target) {
-      console.log('target', target);
-    }
-
-    function other(target) {
-      console.log('other', target);
-    }
-
-    // function firstFilter(target) {
-    //   console.log(postsSBArr.filter(function(target){
-    //     return ;
-    //   }));
-    // }
-
-    // function trueFilter(target) {
-    //   let targetArr = [target]
-    //   console.log(postsSBArr.filter(function(target){
-    //     let targetResult = targetArr.some(postsSBArr);
-    //     return targetResult
-    //     // return el.price <= 1000 &&
-    //     //  el.sqft >= 500 &&
-    //     //  el.num_of_beds >=2 &&
-    //     //  el.num_of_baths >= 2.5;
-    //   },
-    function trueFilter(target) {
-
-      console.log(postsSBArr)
-      }
-
-
-
     console.log('posts passed now logged from Sidebar.js', postsSBArr);
-
+    // <li ('April'),
     return (
       <div className="app-aside-nav">
         <div className="para">
           <p>Archive</p>
             <ul>
-              <li className="years" onClick={() => {filter(2017)}}>2017</li>
+              <li>2017</li>
               <ul className="months">
-                <li onClick={() => {filter('April'), trueFilter('April')}}>April</li>
-                <li onClick={() => {filter('March')}}>March</li>
-                <li onClick={() => {filter('February')}}>February</li>
-                <li onClick={() => {filter('January'), other('January')}}>January</li>
+                <li>April</li>
+                <li>March</li>
+                <li>February</li>
+                <li>January</li>
               </ul>
-              <li className="years" onClick={() => {filter(2016)}}>2016</li>
+              <li>2016</li>
               <ul className="months">
-                <li onClick={() => {filter('December'), trueFilter('December')}}>December</li>
+                <li>December</li>
               </ul>
             </ul>
           <p>Tags</p>
