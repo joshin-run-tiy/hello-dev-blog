22bb9827c4e6530ccf3d90e269c24d635e51dfdb
joshin-run <jahmsc@gmail.com>
2017-06-01 07:31:43 -0600

    Feat (firebase) Attempt to integrate firebase; filled firebase with two post entries, placed firebase code, did not produce result yet

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/app.css';
 import Sidebar from './components/Sidebar'
-import Blog from './components/Blog'
+// import Blog from './components/Blog'
 import Links from './components/Links'
 import About from './components/About'
 import Footer from './components/Footer'
 import Post from './components/Post'
-import Posts from './components/Posts'
+// import Posts from './components/Posts'
+import * as firebase from 'firebase';
 
 class App extends React.Component {
 
@@ -15,21 +16,45 @@ class App extends React.Component {
     super(props)
     this.state = {
       screenIndex: 1,
-      posts: Posts
-    }
+        other: "Hello World!"
+      // posts: Posts
+    };
   }
 
   updateScreen(newScreenIndex) {
     this.setState({
-      screenIndex: newScreenIndex
+      screenIndex: newScreenIndex,
+      other: "Blank"
+    });
+  }
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref().child('react');
+    const otherRef = rootRef.child('other');
+    otherRef.on('value', snap => {
+      this.setState({
+        other: snap.val()
+      });
     });
   }
 
   render() {
-    let ActiveScreen
-    <Sidebar posts={this.state.posts.posts}/>
+
+    let config = {
+      apiKey: "AIzaSyD-VvpgfQQSDwL-HQKTQqXvgz-R3HzoJck",
+      authDomain: "hello-dev-24ebe.firebaseapp.com",
+      databaseURL: "https://hello-dev-24ebe.firebaseio.com",
+      projectId: "hello-dev-24ebe",
+      storageBucket: "hello-dev-24ebe.appspot.com",
+      messagingSenderId: "368529034774"
+    };
+    firebase.initializeApp(config);
+
+    let ActiveScreen;
+    // <Sidebar posts={this.state.posts.posts}/>
     if(this.state.screenIndex === 1) {
-      ActiveScreen = <Blog posts={this.state.posts.posts}/>
+      ActiveScreen = "hello ActiveScreen"
+      // ActiveScreen = <Blog posts={this.state.posts.posts}/>
     }
     if(this.state.screenIndex === 2) {
       ActiveScreen = <Links />
